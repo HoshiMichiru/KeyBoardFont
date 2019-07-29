fa802e4f12a07b3071b156717e24c3bced6814d6
Michiru <starffhwxx@gmil.com>
2019-07-29 10:39:13 +0800

    キーボードとフォントのライブラリ追加済み

---
+++ IQKeyboardManagerSwiftProject/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  IQKeyboardManagerSwiftProject
+//
+//  Created by 星みちる on 2019/07/29.
+//  Copyright © 2019 星みちる. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
